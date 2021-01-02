fbef84c04120349d366dd344d805ad278b287874
Pedro Mello <pedrocmello.bsi@gmail.com>
2021-01-02 15:07:55 -0300

    updating readme.md

--- components/about.js
+++ components/about.js
@@ -14,7 +14,7 @@ const About = () => (
 
     <div className="p-4 md:p-12 text-center lg:text-left">
       <div className="block rounded-full shadow-xl mx-auto -mt-16 h-48 w-48 bg-cover bg-center">
-        <RoundImg src="/assets/blog/authors/cicatriz.jpg"/>
+        <RoundImg src="/assets/blog/authors/cicatriz.jpeg"/>
       </div>
     
       <h1 className="text-3xl font-bold pt-8 lg:pt-0">Pedro Mello</h1>
